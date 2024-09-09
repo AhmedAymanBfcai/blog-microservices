b0d443da62d00a73fa6a7afb4c861180014e4e0c
AhmedAymanBfcai <ahmedaymanbfcai123@gmail.com>
2024-09-09 10:06:22 +0300

    Updating Comment Content

--- query/index.js
+++ query/index.js
@@ -43,6 +43,18 @@ app.post("/events", async (req, res) => {
     post.comments.push({ id, content, status });
   }
 
+  if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
+  }
+
   req.send({});
 });
 
